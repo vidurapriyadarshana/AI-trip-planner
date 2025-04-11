9b0ced416cfb747f42e35ef0e9363342105c5cd2
Vidura Priyadarshana <vidura200427@gmail.com>
2025-04-11 16:13:41 +0530

    packages set

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import { Button } from "@/components/ui/button"
+import Hero from "./components/custom/Hero"
 
 function App() {
   return (
-    <div>App</div>
+    <>
+      <Hero />
+    </>
   )
 }
 
---
+++ src/components/custom/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <img src="/logo.svg" alt="" />
+    </div>
+  )
+}
+
+export default Header
