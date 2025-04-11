f1f8801d79cb9b901b4b11ccd6e23c774bc52201
Vidura Priyadarshana <vidura200427@gmail.com>
2025-04-11 16:35:12 +0530

    Landing Page update

--- src/components/custom/Hero.jsx
+++ src/components/custom/Hero.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
+import { Button } from "../ui/button";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <div className="flex flex-col items-center mx-56 gap-9">
+      <h1 className="font-extrabold text-[36px] text-center mt-16">
+        <span className="text-[#f56551]">Discover Your Next Adventure with AI:</span> Personalized Itineraries at Your Fingertips
+      </h1>
+      <p className="text-xl text-gray-500 text-center text-[15px]" >Your personal trip planner and travel curator, creating custom itineraries tailored to your interests and budget.</p>
+      <Button> Get Started, It's Free</Button>
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
