5644b9ffffa686b3c9cbcd645850390945f5684b
Vidura Priyadarshana <vidura200427@gmail.com>
2025-04-11 16:01:04 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button"
+
+function App() {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,24 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import CreateTrip from './create-trip/CreateTrip.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/create-trip",
+    element: <CreateTrip />,
+  }
+  
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
