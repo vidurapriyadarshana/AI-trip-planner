1aa1eba7dd11443d0eaeec2cb8ab2649935d7b5d
Vidura Priyadarshana <vidura200427@gmail.com>
2025-04-11 16:16:57 +0530

    Header Complete

--- src/components/custom/Header.jsx
+++ src/components/custom/Header.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
+import { Button } from "../ui/button";
 
 const Header = () => {
   return (
-    <div>
+    <div className="p-3 shadow-sm flex justify-between items-center px-5">
       <img src="/logo.svg" alt="" />
-    </div>
-  )
-}
 
-export default Header
+      <div >
+        <Button>Sign In</Button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
